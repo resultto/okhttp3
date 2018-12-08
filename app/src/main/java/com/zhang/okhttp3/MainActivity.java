38364a338a96f8fc4aeaa8d5461c705161deafd9
zhangchong <sazjc@sina.com>
2018-12-08 14:21:07 +0800

    1,测试

--- app/src/main/java/com/zhang/okhttp3/MainActivity.java
+++ app/src/main/java/com/zhang/okhttp3/MainActivity.java
@@ -1,42 +1,54 @@
 package com.zhang.okhttp3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    OkHttpClient mOkHttpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpClient mOkHttpClient = new OkHttpClient();
+
+        mOkHttpClient = new OkHttpClient().newBuilder().build();
+
+        TextView tvSync = findViewById(R.id.tv_sync);
+        tvSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request request = new Request.Builder().url("").build();
+                try {
+                    Response execute = mOkHttpClient.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         OkHttpClient build = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder().url("").build();
-
-        try {
-            Response execute = mOkHttpClient.newCall(request).execute();
-            mOkHttpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-
-                }
-            });
-        } catch (IOException e) {
 
 
-        }
+//            mOkHttpClient.newCall(request).enqueue(new Callback() {
+//                @Override
+//                public void onFailure(Call call, IOException e) {
+//
+//                }
+//
+//                @Override
+//                public void onResponse(Call call, Response response) throws IOException {
+//
+//                }
+//            });
+
+
     }
 }
